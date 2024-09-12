1435f85f1c86b37750bdc5fc85cb5dd0de10be57
Krisjward5 <169105340+Krisjward5@users.noreply.github.com>
2024-09-12 14:46:34 +0000

    working

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,22 +1,39 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request, jsonify, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from flask_cors import CORS
 
 api = Blueprint('api', __name__)
+bcrypt = Bcrypt()
 
 # Allow CORS requests to this API
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# @api.route('/hello', methods=['POST', 'GET'])
+# def handle_hello():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+#     response_body = {
+#         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+#     }
 
-    return jsonify(response_body), 200
+#     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    user = User.query.filter_by(email=data['email']).first()
+    if user:
+        return jsonify({"msg":"email already exists"}), 400
+    
+    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    new_user = User(email=data['email'], password = hashed_password)
+
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg": "user created successfully"}), 201
