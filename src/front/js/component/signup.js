dcc419e0bce0ef2ce3b2b36c8c8a1859180b4bcd
Krisjward5 <169105340+Krisjward5@users.noreply.github.com>
2024-09-24 21:52:19 +0000

    working

--- src/front/js/component/signup.js
+++ src/front/js/component/signup.js
@@ -1,41 +1,52 @@
-import React, {useState} from "react";
+import React, { useContext, useState } from "react";
 import axios from "axios";
+import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Signup = () => {
+    const { store, actions } = useContext(Context);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [message, setMessage] = useState('');
+    // const [message, setMessage] = useState('');
 
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post(process.env.BACKEND_URL + "/signup", {email, password}); 
-            setMessage(response.data.msg);
-        } catch (error) {
-            setMessage(error.response.data.msg);
-        } 
-    }; 
+    const navigate = useNavigate;
+
+    function sumbitRequest() {
+        actions.addUser(email, password);
+    }
+
+
     return (
-        <div>
-            <h2>Signup</h2>
-            <form onSubmit={handleSubmit}>
+        <form>
+            <div className="mb-3">
+                <label for="email" className="form-label">Email Address</label>
                 <input
                     type="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     placeholder="Email"
+                    class="form-control" id="email" aria-describedby="emailHelp"
                     required
                 />
+            </div>
+            <div className="mb-3">
+                <label for="password" className="form-label">Password</label>
                 <input
                     type="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Password"
+                    placeholder="Email"
+                    class="form-control" id="password" aria-describedby="passwordHelp"
                     required
                 />
-                <button type="submit">Signup</button>
-            </form>
-            <p>{message}</p>
-        </div>
+            </div>
+            <div id="passwordHelp" className="form-text">
+                We will never share your password with anyone!
+            </div>
+            <Link to="/login">
+                <button type="submit" className="btn btn-primary" onClick={sumbitRequest}>Sign Up</button>
+            </Link>
+        </form>
     )
 }
